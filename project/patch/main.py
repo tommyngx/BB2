b432e807159c31b8e1c223d6e1195244f9e342e9
tommyngx <tommyfederation@gmail.com>
2025-07-31 22:31:25 +1000

    update choices

--- project/patch/main.py
+++ project/patch/main.py
@@ -310,7 +310,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--arch_type",
         type=str,
-        choices=["patch_resnet", "patch_transformer"],
+        choices=["patch_resnet", "patch_transformer", "token_mixer"],
         default=None,
         help="Architecture type: patch_resnet or patch_transformer",
     )
